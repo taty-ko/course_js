744b0811a1c2ac8175d7b25cebaaf7f467ffa8e7
taty-ko <hrkolesnik@gmail.com>
2022-05-31 15:58:33 +0300

    Closures lecture added

--- 19-closures/script.js
+++ 19-closures/script.js
@@ -28,3 +28,23 @@ logNumber(); debugger
 
 
 // Создаем счетчик
+
+function createCounter() {debugger
+    let counter = 0;
+    
+    const myFunction = function() {debugger
+        counter = counter + 1;debugger
+        return counter;debugger
+    }
+return myFunction; debugger
+}
+
+const increment = createCounter();debugger
+
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
+console.log(c1, c2, c3);
+
+
+ 
