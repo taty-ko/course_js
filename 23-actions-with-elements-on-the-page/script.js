068f4bb343b4f71b8c4ab3225d62204e59cf0408
taty-ko <hrkolesnik@gmail.com>
2022-06-06 19:57:10 +0300

    Added Remove, Prepend, Append, before, after, ReplaceWith

--- 23-actions-with-elements-on-the-page/script.js
+++ 23-actions-with-elements-on-the-page/script.js
@@ -4,7 +4,8 @@ const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circles = document.getElementsByClassName('circle'),
       hearts = document.querySelectorAll('.heart'),
-      oneheart = document.querySelector('.heart');
+      oneheart = document.querySelector('.heart')/* ,
+      wrapper = document.querySelector('.wrapper') */;
 
 console.dir(box);   //Выводим в коноь все возможные методы, которые можно делать с контейнером вох.
 
@@ -33,3 +34,43 @@ hearts.forEach(item => {
 
 //добавляем свойство в style.css, текстом.
 box.style.cssText = 'background-color: green; width: 500px';
+
+
+//APPEND - Создаем элементы самостоятельно и помещаем в конец родительского элемента 
+const div = document.createElement('div');
+div.classList.add('.black');
+
+document.body.append(div);// берем родителя body и в самый низ, добаляем нужный нам конетйнер. В нашем случае это div c классом .black
+
+
+// елемент APPLE помещаем вниз, вовнутрь родителя элемента wrapper.
+const apple = document.createElement('div');
+apple.classList.add('.apple');
+apple.style.backgroundColor = '#0ae70a';
+apple.style.width = '20px';
+apple.style.height = '20px';
+apple.style.borderRadius = '50%';
+apple.style.display = 'inline-block';
+document.querySelector('.wrapper').append(apple); // мы не выносили wrapper в отдельную переменную.
+/* const text = document.createTextNode('Here I am :)')
+ */
+
+//помещаем элемент в начало родителького элемента - PREPEND
+document.querySelector('.wrapper').prepend(apple); // мы не выносили wrapper в отдельную переменную.
+document.body.prepend(apple);
+
+
+// before, after
+hearts[1].before(apple);//Добавляем элемент apple ПЕРЕД вторым элементом.
+hearts[1].after(apple);//Добавляем элемент apple ПОСЛЕ второго элементом.
+
+/* hearts.insertBefore(apple, hearts[2]);//устаренвший метод, вставить APPLE в элемент HEARTS перед 3тим элементом
+ */
+// Удаление элементов - REMOVE
+circles[1].remove(); //Удалим 2й кружечек
+/* circles.removeChild(hearts[1]); удаляем 2й кружечек
+ */
+
+hearts[0].replaceWith(circles[0]); //1й элемент hearts заменяем на 1й элемент circles.
+
+
