fa291dba0b0b0b136ec4f8b486e9f259a9c8edd1
taty-ko <hrkolesnik@gmail.com>
2022-06-07 16:21:44 +0300

    Added task: action with DOM on the real project. ForEach, Object, sort, innerHTML, remove.

--- 24-project/js/script.js
+++ 24-project/js/script.js
@@ -24,3 +24,33 @@ const movieDB = {
     ]
 };
 
+const ad = document.querySelectorAll('.promo__adv img');
+ad.forEach(img => {
+    img.remove();
+}); // Удаляем картинки и оставляем беграунд.
+
+// ad.forEach(function (img) {
+//     img.remove();
+// });
+
+const adBox = document.querySelectorAll('.promo__adv');
+adBox.forEach(item => {
+    item.remove();
+}); // Удаляем весь правый столбец контента.
+
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+genre.textContent = 'драма'; //
+poster.style.background = 'url(../img/bg.jpg) center center/cover no-repeat';
+
+const filmList = document.querySelector('.promo__interactive-list');
+filmList.innerHTML = ''; // Очистили элемент, добавив пустую строку.
+
+
+movieDB.movies.sort(); //Отсортировали объект по алфавиту.
+
+movieDB.movies.forEach((film, i) => {
+    filmList.innerHTML += `<li class="promo__interactive-item"> ${i + 1} ${film}
+                                <div class="delete"></div>
+                            </li>`;
+});
