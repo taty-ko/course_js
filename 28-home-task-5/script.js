1755a39968f5b1b99904880b8e7003ff7d176aa3
taty-ko <hrkolesnik@gmail.com>
2022-06-10 11:52:41 +0300

    Added 2 additional expl for cycle and recursion

--- 28-home-task-5/script.js
+++ 28-home-task-5/script.js
@@ -24,3 +24,42 @@ function factirialRec(n, result) {
     }
 }
 console.log(factirialRec(4, 1));
+
+
+// Другие примеры.
+
+let t = 0;
+function f1() {
+    t++;
+    console.log(t);
+
+    if (t === 100) return;
+    f1();
+}
+/* f1(); */
+
+// Цикл
+function f2 () {
+    let out = '';
+    for (let i = 1; i <= 30; i++) {
+        out += i + ' ';
+    }
+    console.log(out);
+}
+f2();
+
+
+//Рекурсия
+
+let i = 0;
+let out = '';
+function f3() {
+    i++;
+    out += i + ' ';
+
+    if(i >= 30) return;
+    f3();
+}
+f3();console.log(out);
+
+
