c0ca4e91061c3f5d9a02390ae07759ff6fb29889
taty-ko <hrkolesnik@gmail.com>
2022-06-21 17:13:58 +0300

    Added the script for show and hide menu, fade animation. Linked every tab and description for showing and hiding content.

--- food-project-gulp/dist/js/show-hide.js
+++ food-project-gulp/dist/js/show-hide.js
@@ -12,8 +12,10 @@ window.addEventListener('DOMContentLoaded', () => {
       function hideTabContent() {
         tabsContent.forEach(item => {     //Поскольку tabsContent - это псевдомассив, поэтому перебираем его с помощью forEach
                                           // аргумент item - называем мы сами, как угодно. в Нашем случае 'item'
-          item.style.display = 'none';    // каждому айтему, каждому блоку в цикле бы присваиваем свойство стиля  - display: none; 
-                                          // В данном случе мы скрыли ВЕСЬ контект блока с сайта
+/*           item.style.display = 'none';    // каждому айтему, каждому блоку в цикле бы присваиваем свойство стиля  - display: none; 
+                                          // В данном случе мы скрыли ВЕСЬ контект блока с сайта */
+             item.classList.add('hide'); // добавляем к элементу перебора класс, и скрываем его
+             item.classList.remove('show', 'fade');//   добавляем к элементу перебора класс, и показываем его          
         });
           
   
@@ -27,8 +29,12 @@ window.addEventListener('DOMContentLoaded', () => {
       // создаем функцию, которая будет показывать нам табы
       function showTabContent(i = 0) {          // т.к function showTabContent(i) от ай, то мы хотим подсветить тот элемент (по дефолту), номер которого укзан в аргументе функции. В даном случе 0 - первый.
                                             // и этот элемент попадает в функцию  function showTabContent(i) и у него появляется стиль дисплей блок.
-        tabsContent[i].style.display = 'block'; // і - номер по порядку нашего блока, который показываем. Показываем его с помощью изменения свойства стила - displey: block
-        tabs[i].classList.add('tabheader__item_active'); // используем метод add. В каждом элементе tab, мы добавляем класс активности tabheader__item_active. Обратная операция строке 123-125
+       /*  tabsContent[i].style.display = 'block'; // і - номер по порядку нашего блока, который показываем. Показываем его с помощью изменения свойства стила - displey: block */
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active'); // используем метод add. В каждом элементе tab, мы добавляем класс активности tabheader__item_active. Обратная операция строке 123-125 
+
+        
       }
   
       hideTabContent();
@@ -38,18 +44,20 @@ window.addEventListener('DOMContentLoaded', () => {
       // Используем делегирование событий и используем обработчик клика по табу.ы
       tabsParent.addEventListener('click', (event) => { //event - объект события
         const target = event.target; //это «целевой» элемент, на котором произошло событие, в процессе всплытия он неизменен. Т.е это объект - инициатор события
+        console.log('click works');
 
-            if (target && target.classList.contains('.tabheader__item')) {  //если при клике есть родитель .tabheader__items (tabsParent) И есть дочерний элемент .tabheader__item, то
+            if (target && target.classList.contains('tabheader__item')) {  //если при клике есть родитель .tabheader__items (tabsParent) И есть дочерний элемент .tabheader__item, то
                 tabs.forEach((item, i) => { // мы перебираем псевдомассив tabs и ищем совпадения, которые совпадают с условием, т.е ищем класс, на который клацнул пользователь.
                                                 // item - каждый таб, который будем перебирать
                                                 // i - номер элемента по порядку, который перебираем.
-                    if(target == item) // если target, т.е кликабельное поле, совпадает (==) с номером элемента, то мы бйдем вызывать наши 2 функции  hideTabContent(); showTabContent();                   
-                    hideTabContent(); // скрываем все блоки. кроме того, что і, на который кликнул пользователь.
-                    showTabContent(i);  // показывает номер элемента, на который кликнули. Кликнули на 3, то и і будет 2.
-                    console.log('jfjf');
+                    if (target == item) {// если target, т.е кликабельное поле, совпадает (==) с номером элемента, то мы бйдем вызывать наши 2 функции  hideTabContent(); showTabContent();                   
+                    
+                      hideTabContent(); // скрываем все блоки. кроме того, что і, на который кликнул пользователь.
+                      showTabContent(i);
+                    }  // показывает номер элемента, на который кликнули. Кликнули на 3, то и і будет 2.
                 });
             }
 
-      })
+      });
   });
   
