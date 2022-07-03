836b99aa8a0cbdbd1104748b1b9e24a9eaf40d8f
taty-ko <hrkolesnik@gmail.com>
2022-07-03 16:50:14 +0300

    Added Rest operator for crastiong more classes for menu cards.

--- food-project-gulp/dist/js/classes-cards.js
+++ food-project-gulp/dist/js/classes-cards.js
@@ -5,13 +5,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Создаем шаблон - класс и от него будет отпочковываться.
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             // прописывеем все свойства, которые нужны для создания карточки (взаимодействия и динамического изменения)
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector); //положили дом-элемент, родитель.
             this.transfer = 27;
             this.changeToUah();
@@ -27,18 +28,26 @@ window.addEventListener('DOMContentLoaded', () => {
         render() {
             const element = document.createElement('div'); // создаем элемент div.
 
+            if(this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className)); // для каждого нового элемента, который мы добавим в div карточки
+                                                                                     // мы добавляем каждому элементу название класса - classList.add(className)
+
+            }
+
+           
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt} />
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">
-                    ${this.descr}
-                    </div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt} />
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">
+                ${this.descr}
+                </div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
             this.parent.append(element);
@@ -56,7 +65,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         9 ,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -65,7 +75,9 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Постное”',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         7 ,
-        '.menu .container'
+        '.menu .container',
+        /* 'menu__item',
+        'big' */
     ).render();
 
     new MenuCard(
@@ -74,7 +86,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Фитнес”',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         5 ,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
 });
