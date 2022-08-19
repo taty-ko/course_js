a5d60b089e553a3be1ad971c3885ee351090d60e
taty-ko <hrkolesnik@gmail.com>
2022-08-19 14:05:36 +0200

    Added Promise

--- 45-promise/script.js
+++ 45-promise/script.js
@@ -1,2 +1,42 @@
 'use strict';
 //Promise
+
+const test = time => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            
+            resolve();
+        }, time)
+    })
+};
+/* 
+test(1000).then(() => {
+    console.log('hi через 1000 ms');
+}); 
+
+test(3000).then(() => {
+    console.log('hi через 3000 ms');
+});  */
+
+Promise.all([test(1000), test(2000), test(3000), test(4000)]).then(() => {
+    console.log('All');
+});
+
+Promise.race([test(1000), test(2000), test(3000), test(4000)]).then(() => {
+    console.log('Race');
+});
+
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        name: 'Taty'
+    }),
+    headers: {
+        'Content-type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(json => console.log(json));
+
+  
