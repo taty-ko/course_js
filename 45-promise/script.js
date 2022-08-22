554757fdedf20ee175e35be6eb594a310e72ade7
taty-ko <hrkolesnik@gmail.com>
2022-08-22 12:41:07 +0200

    Added test task for Promise.

--- 45-promise/script.js
+++ 45-promise/script.js
@@ -40,3 +40,55 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 .then(json => console.log(json));
 
   
+
+const delay = ms => {
+ return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+        resolve()
+    }, ms);
+ }) 
+} 
+  
+  delay(3000)
+  .then(() => console.log('выполнилось через 3 секунды'));
+  
+
+
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+
+//
+
+
+  showCircle(150, 150, 100).then(div => {
+    div.classList.add('message-ball');
+    div.append("Hello, world!");
+  });
+
+
+  function showCircle(cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+
+        div.addEventListener('transitionend', function handler() {
+          div.removeEventListener('transitionend', handler);
+          resolve(div);
+        });
+      }, 0);
+    })
+  }
+  
